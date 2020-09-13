c305efa3a9eac7b626bee465d7ed07d2fc2f3cd7
Ido <iporat08@gmail.com>
2020-09-13 19:38:36 +0300

    can now know current location

--- GetLOcationFromMapExample/app/src/main/java/com/example/getlocationfrommapexample/MainActivity.java
+++ GetLOcationFromMapExample/app/src/main/java/com/example/getlocationfrommapexample/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.getlocationfrommapexample;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,8 +29,19 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
 
-import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Locale;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -33,6 +50,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     public static final float DEFAULT_RADIUS = 500;
     public static final int RADIUS_MAX_IN_METERS = 5000;
     public static final String CENTER_MARKER = "centerMarker";
+    private static final String TAG = "tag";
+    public static final int LOCATION_PERMISSIONS_REQUEST_CODE = 5;
 
     private LatLng centerLatLng = new LatLng(31.772915, 35.218016);
     private Circle mapCircle;
@@ -40,10 +59,12 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     private SeekBar radiusSeekBar;
     private int seekBerMax;
     private float radius = DEFAULT_RADIUS;
+    private FusedLocationProviderClient fusedLocationClient;
 
     GoogleMap mapAPI;
     SupportMapFragment mapFragment;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,7 +74,57 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
         setSeekBar();
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (hasLocationPermissions()) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSIONS_REQUEST_CODE);
+            return;
+        }
+        getLastLocation();
+
     }
+
+    private void getLastLocation() {
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            centerLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+                            if(centerMarker != null){
+                                centerMarker.remove();
+                            }
+                            centerMarker = mapAPI.addMarker(
+                                    new MarkerOptions().position(centerLatLng).draggable(true).title(CENTER_MARKER));
+                            mapCircle.setCenter(centerLatLng);
+                            mapAPI.moveCamera(CameraUpdateFactory.newLatLngZoom(centerLatLng, 14.0f));
+                            mapAPI.getUiSettings().setZoomControlsEnabled(true);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if(requestCode == LOCATION_PERMISSIONS_REQUEST_CODE){
+                getLastLocation();
+            }
+        }
+    }
+
+    private boolean hasLocationPermissions() {
+        return ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED;
+    }
+
 
     private void setSeekBar() {
         radiusSeekBar = (SeekBar) findViewById(R.id.RadiusSeekBar);
@@ -115,6 +186,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void initializeCenterAndCircle() {
+
         centerMarker = mapAPI.addMarker(
                 new MarkerOptions().position(centerLatLng).draggable(true).title(CENTER_MARKER));
 
