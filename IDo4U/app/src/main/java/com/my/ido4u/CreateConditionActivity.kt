e3772c21e96c32e66231daa0412c8e7206ddd2b0
IdoLazer <idolazer8@gmail.com>
2020-09-20 11:16:07 +0300

    added CreateConditionActivity

---
+++ IDo4U/app/src/main/java/com/my/ido4u/CreateConditionActivity.kt
@@ -0,0 +1,11 @@
+package com.my.ido4u
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateConditionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_condition)
+    }
+}
